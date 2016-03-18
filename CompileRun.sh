1fe9573869cd4f23d64090a51a488378a74c5d13
Parshuram <parushuram.k@huawei.com>
2016-03-18 14:51:55 +0000

    Shell script to build and run the module

---
+++ CompileRun.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export MESOS_HOME_DIR="/home/ubuntu/git_mesos/mesos"
+
+echo $MESOS_HOME_DIR
+
+COMPILER="g++"
+LP_THIRD_PARTY="$MESOS_HOME_DIR/build/3rdparty/libprocess/3rdparty"
+
+HEADER_FILES=" -I $MESOS_HOME_DIR/include -I $MESOS_HOME_DIR/src -I $MESOS_HOME_DIR/build/src -I $MESOS_HOME_DIR/3rdparty/libprocess/include -I $LP_THIRD_PARTY/boost-1.53.0 -I $LP_THIRD_PARTY/glog-0.3.3/src -I $LP_THIRD_PARTY/stout/include -I $LP_THIRD_PARTY/protobuf-2.5.0/src"
+
+
+$COMPILER $HEADER_FILES -lmesos -std=c++11 -fPIC -shared FedCommunication.cpp -o libFedCommunicationModule.so 
+
+$MESOS_HOME_DIR/build/src/.libs/lt-mesos-master --ip=127.0.0.1 --work_dir=$HOME --modules="file://$PWD/jFedCommunication.json"
