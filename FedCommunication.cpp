7c86f41a239ac0e66a5ef764cde3328a69210d8a
Parshuram <parushuram.k@huawei.com>
2016-03-18 14:47:32 +0000

    Initial copy of FedCommunication class defination with module creation

---
+++ FedCommunication.cpp
@@ -0,0 +1,21 @@
+#include "FedCommunication.hpp"
+
+
+static Anonymous* createCommunication(const Parameters& parameters)
+{
+  auto obj = new Communicate();
+
+  return obj;
+}
+
+
+// Declares an Anonymous module named 'org_apache_mesos_TestAnonymous'.
+mesos::modules::Module<Anonymous> mesos_federation_communication (
+  MESOS_MODULE_API_VERSION,
+  MESOS_VERSION,
+  "Huawei Mesos Federation",
+  "parushuram.k@huawei.com",
+  "Mesos Federation Communication Module.",
+  NULL,
+  createCommunication);
+
